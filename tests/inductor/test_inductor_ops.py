4f4a4d6b6e3291ba38eaa3dae0c7c7a65294f9ba
Hayden Narey <hayden.narey@ibm.com>
2026-09-14 15:18:58 +0000

    test: assert no FallbackWarning in test_triu_cpu
    Signed-off-by: Hayden Narey <hayden.narey@ibm.com>

--- tests/inductor/test_inductor_ops.py
+++ tests/inductor/test_inductor_ops.py
@@ -7435,6 +7435,7 @@ class TestOps(unittest.TestCase, metaclass=ParameterizedTestMeta):
 
         self.compare_with_cpu(fn, x)
 
+    @pytest.mark.filterwarnings("error::torch_spyre.ops.fallbacks.FallbackWarning")
     def test_triu_cpu(self, x, diagonal):
         def fn(input, diagonal):
             return torch.triu(input, diagonal)
